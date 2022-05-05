ff43ba2513640ab1fb4258570e6eff9caf62e290
John Mathews <johnmatthews4@wayfair.com>
2022-05-05 17:04:02 +0200

    basic tests

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
-from typing import Optional
+from typing import Dict, List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    uid: str
+    message: str
+    variables: List[Dict[str, str]]
+    paper_type: str
+    paper_size: str
+    paper_orientation: str
+
 
 app = FastAPI()
 
 
+# demo
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# https://fastapi.tiangolo.com/tutorial/body/
+@app.post("/api/v1/create/")
+async def create_item(item: Item):
+    return item
